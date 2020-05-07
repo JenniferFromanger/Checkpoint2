d4e1de75e543566309422103597ff0295b2c5dbf
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-05-07 10:22:20 +0200

    add a litle CSS and clip

--- list-games/src/components/Game.jsx
+++ list-games/src/components/Game.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import "./Game.scss";
 
 export default function Game(props) {
   return (
-    <div>
-      {" "}
-      <h1>{props.name}</h1>
-      <p>{props.released}</p>
-      <img src={props.background_image} alt={props.name} />
-      <p></p>{" "}
+    <div className="contain">
+      <div className="align">
+        <h2>{props.name}</h2>
+        <p>Released on {props.released}</p>
+      </div>
+      <div className="align">
+        <img src={props.background_image} alt={props.name} />{" "}
+        <div>
+          <p>Listen the clip's game</p>
+          <iframe src={props.clip.clip} title={props.id} />
+        </div>
+      </div>
     </div>
   );
 }
